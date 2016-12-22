73a3b951b716aac492a7063c010794d0fe0bb201
iotvl <vladik2395@gmail.com>
2016-12-22 21:18:32 +0200

    new lab4

--- lab4.cpp
+++ lab4.cpp
@@ -3,196 +3,193 @@
 
 using namespace std;
 
-class Geometric
+class GeometricFigures
 {
 public:
 	virtual double Square()
 	{
 		return 0;
 	}
-	virtual int Get()
+	virtual int InputSpecifications()
 	{
 		return 0;
 	}
 };
-	class D2 : public Geometric
+class TwoDimensionalFigures : public GeometricFigures
+{
+public:
+	virtual double Square()
 	{
-	public:
-		virtual double Square() 
-		{
-			return 0;
-		}
-		virtual int Get()
-		{
-			return 0;
-		}
-	};
-
-		class  Circle : public D2
-		{
-		public:
-			int Get()
-			{
-				cout << "Vvedu radius: ";
-				cin >> r;
-				return 0;
-			}
-			double Square()
-			{
-				return 3.14*r*r;
-			}
-		private:
-			int r;
-		};
-
-		class Triangle : public D2
-		{
-		public:
-			int Get()
-			{
-				cout << "Vvedu storony: ";
-				cin >> a;
-				cout << "Vvedu vusoty: ";
-				cin >> h;
-				return 0;
-			}
-			double Square()
-			{
-				return 0.5*a*h;
-			}
-		private:
-			int a, h;
-		};
-
-	class D3 : public Geometric
+		return 0;
+	}
+	virtual int InputSpecifications()
 	{
-	public:
-		virtual int Get()
-		{
-			return 0;
-		}
-		virtual double Square()
-		{
-			return 0;
-		}
-		virtual double Extend()
-		{
-			return 0;
-		}
-	};
+		return 0;
+	}
+};
+class  Circle : public TwoDimensionalFigures
+{
+public:
+	int InputSpecifications()
+	{
+		cout << "Enter radius: ";
+		cin >> r;
+		return 0;
+	}
+	double Square()
+	{
+		return 3.14*r*r;
+	}
+private:
+	int r;
+};
+class Triangle : public TwoDimensionalFigures
+{
+public:
+	int InputSpecifications()
+	{
+		cout << "Enter side: ";
+		cin >> a;
+		cout << "Enter height: ";
+		cin >> h;
+		return 0;
+	}
+	double Square()
+	{
+		return 0.5*a*h;
+	}
+private:
+	int a, h;
+};
+class ThreeDimensionalFigures : public GeometricFigures
+{
+public:
+	virtual int InputSpecifications()
+	{
+		return 0;
+	}
+	virtual double Square()
+	{
+		return 0;
+	}
+	virtual double Volume()
+	{
+		return 0;
+	}
+};
 
-		class Cylinder : public D3
-		{
-		public:
-			int Get()
-			{
-				cout << "Vvedu radius: ";
-				cin >> r;
-				cout << "Vvedu vusoty: ";
-				cin >> h;
-				return 0;
-			}
-			double Square()
-			{
-				return 3.14*r*r;
-			}
-			double Extend()
-			{
-				return 3.14*r*r*h;
-			}
-		private:
-			int r, h;
-		};
+class Cylinder : public ThreeDimensionalFigures
+{
+public:
+	int InputSpecifications()
+	{
+		cout << "Enter radius: ";
+		cin >> r;
+		cout << "Enter height: ";
+		cin >> h;
+		return 0;
+	}
+	double Square()
+	{
+		return 3.14*r*r;
+	}
+	double Volume()
+	{
+		return 3.14*r*r*h;
+	}
+private:
+	int r, h;
+};
 
-		class Sphere : public D3
-		{
-		public:
-			int Get()
-			{
-				cout << "Vvedu radius: ";
-				cin >> r;
-				return 0;
-			}
-			double Square()
-			{
-				return 4*3.14*r*r;
-			}
+class Sphere : public ThreeDimensionalFigures
+{
+public:
+	int InputSpecifications()
+	{
+		cout << "Enter radius: ";
+		cin >> r;
+		return 0;
+	}
+	double Square()
+	{
+		return 4 * 3.14*r*r;
+	}
 
-			double Extend()
-			{
-				return 4 / 3 * 3.14*r*r*r;
-			}
-		private:
-			int r;
-		};
+	double Volume()
+	{
+		return 4 / 3 * 3.14*r*r*r;
+	}
+private:
+	int r;
+};
 
-		class Cone : public D3
-		{
-		public:
-			int Get()
-			{
-				cout << "Vvedu radius: ";
-				cin >> r;
-				cout << "Vvedu vusoty: ";
-				cin >> h;
-				return 0;
-			}
-			double Square()
-			{
-				return 3.14*r*r;
-			}
-			double Extend()
-			{
-				return 3.14/3*r*r*h;
-			}
-		private:
-			int r, h;
-		};
+class Cone : public ThreeDimensionalFigures
+{
+public:
+	int InputSpecifications()
+	{
+		cout << "Enter radius: ";
+		cin >> r;
+		cout << "Enter height: ";
+		cin >> h;
+		return 0;
+	}
+	double Square()
+	{
+		return 3.14*r*r;
+	}
+	double Volume()
+	{
+		return 3.14 / 3 * r*r*h;
+	}
+private:
+	int r, h;
+};
 
 int main()
 {
-	cout << "Vuberu figyry:" << endl;
-	cout << "1. Kolo" << endl;
-	cout << "2. Trukytnuk" << endl;
+	cout << "Choose figure:" << endl;
+	cout << "1. Circle" << endl;
+	cout << "2. Triangle" << endl;
 	cout << "3. Cylinder" << endl;
 	cout << "4. Sphera" << endl;
-	cout << "5. Konys" << endl;
+	cout << "5. Cone" << endl;
 	int choise;
 	cin >> choise;
 	if (choise == 1)
 	{
-		D2 *ptr1 = new Circle;
-		ptr1->Get();
-		cout << (ptr1->Square()) << endl;
+		TwoDimensionalFigures *pointer1 = new Circle;
+		pointer1->InputSpecifications();
+		cout << "Square = " << (pointer1->Square()) << endl;
 	}
 	else if (choise == 2)
 	{
-		D2 *ptr2 = new Triangle;
-		ptr2->Get();
-		cout << (ptr2->Square())<<endl;
+		TwoDimensionalFigures *pointer2 = new Triangle;
+		pointer2->InputSpecifications();
+		cout << "Square = " << (pointer2->Square()) << endl;
 	}
 	else if (choise == 3)
 	{
-		D3 *ptr3 = new Cylinder;
-		ptr3->Get();
-		cout << (ptr3->Square()) << endl;
-		cout << (ptr3->Extend())<<endl;
+		ThreeDimensionalFigures *pointer3 = new Cylinder;
+		pointer3->InputSpecifications();
+		cout << "Square = " << (pointer3->Square()) << endl;
+		cout << "Volume = " << (pointer3->Volume()) << endl;
 	}
 	else if (choise == 4)
 	{
-		D3 *ptr4 = new Sphere;
-		ptr4->Get();
-		cout << (ptr4->Square()) << endl;
-		cout << (ptr4->Extend()) << endl;
+		ThreeDimensionalFigures *pointer4 = new Sphere;
+		pointer4->InputSpecifications();
+		cout << "Square = " << (pointer4->Square()) << endl;
+		cout << "Volume = " << (pointer4->Volume()) << endl;
 	}
 	else if (choise == 5)
 	{
-		D3 *ptr5 = new Cone;
-		ptr5->Get();
-		cout << (ptr5->Square()) << endl;
-		cout << (ptr5->Extend()) << endl;
+		ThreeDimensionalFigures *pointer5 = new Cone;
+		pointer5->InputSpecifications();
+		cout << "Square = " << (pointer5->Square()) << endl;
+		cout << "Volume = " << (pointer5->Volume()) << endl;
 	}
 	else
-		cout << "Nepravulnuj nomer" <<endl;
-    return 0;
+		cout << "Wrong number" << endl;
+	return 0;
 }
